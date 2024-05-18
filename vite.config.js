7b4680188079765de1e5c36e221e3b654e31e806
Sara Celidonio <saracelidonio@saras-air.myfiosgateway.com>
2024-05-17 23:05:24 -0400

    vite config

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/grandmas-room/', // Replace 'your-repository' with the name of your GitHub repository
+});
