48635bbdfb04a4bc7d5d35c628cccfc9208662b4
Sara Celidonio <saracelidonio@saras-air.myfiosgateway.com>
2024-05-18 00:12:11 -0400

    fix asset build

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/grandmas-room/', // Replace 'your-repository' with the name of your GitHub repository
+  base: '/grandmas-room/', 
+  assetsInclude: ['**/*.hdr','**/*.glb'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['three', 'bootstrap'] // Example of splitting 'three' and 'bootstrap' into a vendor chunk
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit if necessary
+  }
 });
